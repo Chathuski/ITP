a69b7609fb382cf1caf9b078c41b4ff52394c0e2
Chathuski <chathujay92@gmail.com>
2017-08-10 11:46:38 +0530

    Add files via upload

---
+++ SampleProj/src/sampleproj/SampleProj.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleproj;
+
+/**
+ *
+ * @author 2016
+ */
+public class SampleProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World !!!  ");
+    }
+    
+}
